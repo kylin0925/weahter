be30885e4c225e3c255ab0a880be9424eb1264ad
kylin <kylin25@gmail.com>
2018-10-05 17:34:57 +0800

    add icons. change weekly layout and add icon.

--- app/src/main/java/ap/ky/weather/PageViewWeekly.java
+++ app/src/main/java/ap/ky/weather/PageViewWeekly.java
@@ -3,6 +3,7 @@ package ap.ky.weather;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -34,24 +35,74 @@ public class PageViewWeekly extends RelativeLayout {
         TextView txtTemp7 = (TextView) view.findViewById(R.id.txtTemp7);
         textView.setText(location.locationName);
 
+        ImageView ivWeather = (ImageView)view.findViewById(R.id.ivWeather1);
+        ImageView ivWeather1 = (ImageView)view.findViewById(R.id.ivWeather2);
+        ImageView ivWeather2 = (ImageView)view.findViewById(R.id.ivWeather3);
+        ImageView ivWeather3 = (ImageView)view.findViewById(R.id.ivWeather4);
+        ImageView ivWeather4 = (ImageView)view.findViewById(R.id.ivWeather5);
+        ImageView ivWeather5 = (ImageView)view.findViewById(R.id.ivWeather6);
+        ImageView ivWeather6 = (ImageView)view.findViewById(R.id.ivWeather7);
+
+
+        ImageView ivWeather_1 = (ImageView)view.findViewById(R.id.ivweather1_1);
+        ImageView ivWeather2_2 = (ImageView)view.findViewById(R.id.ivweather2_2);
+        ImageView ivWeather3_3 = (ImageView)view.findViewById(R.id.ivweather3_3);
+        ImageView ivWeather4_4 = (ImageView)view.findViewById(R.id.ivweather4_4);
+        ImageView ivWeather5_5 = (ImageView)view.findViewById(R.id.ivweather5_5);
+        ImageView ivWeather6_6 = (ImageView)view.findViewById(R.id.ivweather6_6);
+        ImageView ivWeather7_7 = (ImageView)view.findViewById(R.id.ivweather7_7);
+
+        TextView txtTemp1_1 = (TextView) view.findViewById(R.id.txtTemp_1);
+        TextView txtTemp2_1 = (TextView) view.findViewById(R.id.txtTemp2_1);
+        TextView txtTemp3_1 = (TextView) view.findViewById(R.id.txtTemp3_1);
+        TextView txtTemp4_1 = (TextView) view.findViewById(R.id.txtTemp4_1);
+        TextView txtTemp5_1 = (TextView) view.findViewById(R.id.txtTemp5_1);
+        TextView txtTemp6_1 = (TextView) view.findViewById(R.id.txtTemp6_1);
+        TextView txtTemp7_1 = (TextView) view.findViewById(R.id.txtTemp7_1);
+
         if (location.weatherElement != null) {
             WeatherElement wx = location.weatherElement.get(0);
             WeatherElement maxt = location.weatherElement.get(1);
             WeatherElement mint = location.weatherElement.get(2);
             String[] timelst = new String[7];
             String[] templst = new String[7];
+            String[] templstNight = new String[7];
+            int[] weatherIconlst = new int[7];
+            int[] weatherIconlstNight = new int[7];
+
             for(int i = 0;i<7;i++){
-                timelst[i] = maxt.time.get(i*2).startTime + "\n" +
-                             maxt.time.get(i*2).endTime  ;
+                String startTime = maxt.time.get(i*2).startTime;
+                String endTime = maxt.time.get(i*2).endTime;
+                String day = "";
+                String night = "";
+                // date format 2018-02-11T12:00:00+08:00
+                int tmp;
+                String time = "";
+                tmp = startTime.indexOf('T');
+                time = startTime.substring(tmp+1,startTime.length());
+                startTime = startTime.substring(0,tmp);
+                if(time.substring(0,5).equals("06:00")){
+                    day = startTime + " " + getResources().getString(R.string.weekly_day) ;
+                    tmp = endTime.indexOf('T');
+                    night = endTime.substring(0,tmp) + " " + getResources().getString(R.string.weekly_night) ;
+                }else{
+                    day = startTime + " " + getResources().getString(R.string.weekly_night) ;
+                    tmp = endTime.indexOf('T');
+                    night = endTime.substring(0,tmp) + " " + getResources().getString(R.string.weekly_day) ;
+                }
+
+                timelst[i] = day + " ~ " +  night ;
                              //maxt.time.get(i*2 + 1).startTime + "\n" +
                              //maxt.time.get(i*2 + 1).endTime + "\n";
 
                 templst[i] = mint.time.get(i*2).parameter.parameterName + " " +
-                             maxt.time.get(i*2).parameter.parameterName + " " +
-                             wx.time.get(i*2).parameter.parameterName + "\n" +
-                             mint.time.get(i*2 + 1).parameter.parameterName + " " +
-                             maxt.time.get(i*2 + 1).parameter.parameterName + " " +
-                             wx.time.get(i*2).parameter.parameterName;
+                             maxt.time.get(i*2).parameter.parameterName + "\n" +
+                             wx.time.get(i*2).parameter.parameterName ;
+                templstNight[i] = mint.time.get(i*2 + 1).parameter.parameterName + " " +
+                             maxt.time.get(i*2 + 1).parameter.parameterName + "\n" +
+                             wx.time.get(i*2 + 1).parameter.parameterName;
+                weatherIconlst[i] = WeatherIconMap.iconMap.get( wx.time.get(i*2).parameter.parameterName);
+                weatherIconlstNight[i] = WeatherIconMap.iconMap.get( wx.time.get(i*2+1).parameter.parameterName);
             }
 
             txtTemp1.setText(templst[0]);
@@ -69,6 +120,30 @@ public class PageViewWeekly extends RelativeLayout {
             txtStartTime5.setText(timelst[4]);
             txtStartTime6.setText(timelst[5]);
             txtStartTime7.setText(timelst[6]);
+
+            ivWeather.setImageResource(weatherIconlst[0]);
+            ivWeather1.setImageResource(weatherIconlst[1]);
+            ivWeather2.setImageResource(weatherIconlst[2]);
+            ivWeather3.setImageResource(weatherIconlst[3]);
+            ivWeather4.setImageResource(weatherIconlst[4]);
+            ivWeather5.setImageResource(weatherIconlst[5]);
+            ivWeather6.setImageResource(weatherIconlst[6]);
+
+            ivWeather_1.setImageResource(weatherIconlstNight[0]);
+            ivWeather2_2.setImageResource(weatherIconlstNight[1]);
+            ivWeather3_3.setImageResource(weatherIconlstNight[2]);
+            ivWeather4_4.setImageResource(weatherIconlstNight[3]);
+            ivWeather5_5.setImageResource(weatherIconlstNight[4]);
+            ivWeather6_6.setImageResource(weatherIconlstNight[5]);
+            ivWeather7_7.setImageResource(weatherIconlstNight[6]);
+
+            txtTemp1_1.setText(templstNight[0]);
+            txtTemp2_1.setText(templstNight[1]);
+            txtTemp3_1.setText(templstNight[2]);
+            txtTemp4_1.setText(templstNight[3]);
+            txtTemp5_1.setText(templstNight[4]);
+            txtTemp6_1.setText(templstNight[5]);
+            txtTemp7_1.setText(templstNight[6]);
         }
 
         addView(view);
